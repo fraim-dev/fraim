366380f0a1e5ee8ad845ad70931974bef1892dfa
Greg Climer <greg.climer@resourcely.io>
2025-07-11 10:56:06 -0500

    introduces ProjectInput

---
+++ fraim/inputs/project.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+import tempfile
+from typing import Any, Generator
+from pydantic import BaseModel
+
+from fraim.config.config import Config
+from fraim.core.contextuals.code import CodeChunk
+from fraim.inputs.file_chunks import chunk_input
+from fraim.inputs.files import Files
+from fraim.inputs.git import Git
+from fraim.inputs.local import Local
+
+
+class ProjectInput:
+    config: Config
+    files: Files
+    chunk_size: int
+    project_path: str
+
+    def __init__(self, config: Config, kwargs: Any) -> None:
+        self.config = config
+        path_or_url = kwargs.location or None
+        globs = kwargs.globs
+        limit = kwargs.limit
+        self.chunk_size = kwargs.chunk_size
+
+        if path_or_url is None:
+            raise ValueError("Location is required")
+
+        if path_or_url.startswith("http://") or path_or_url.startswith("https://") or path_or_url.startswith("git@"):
+            temp_dir = tempfile.mkdtemp(prefix="fraim_scan_")
+            self.project_path = os.path.join(temp_dir, "repo")
+            self.config.logger.info(
+                f"Cloning repository: {path_or_url} into path: {self.project_path}")
+            self.files = Git(self.config, url=path_or_url,
+                             tempdir=self.project_path, globs=globs, limit=limit)
+        else:
+            self.project_path = path_or_url
+            self.files = Local(self.config, Path(
+                path_or_url), globs=globs, limit=limit)
+
+    def __iter__(self) -> Generator[CodeChunk, None, None]:
+        for file in self.files:
+            self.config.logger.info(f"Generating chunks for file: {file.path}")
+            chunked = chunk_input(file, self.project_path, self.chunk_size)
+            for chunk in chunked:
+                yield chunk
--- fraim/workflows/application/workflow.py
+++ fraim/workflows/application/workflow.py
@@ -10,12 +10,12 @@ Analyzes application configurations and deployment files for security vulnerabil
 import os
 from dataclasses import dataclass
 from typing import Annotated, Any, List, Optional
+from types import SimpleNamespace
 
 from fraim.config import Config
-from fraim.core.contextuals import CodeChunk
 from fraim.core.workflows import Workflow
+from fraim.inputs.project import ProjectInput
 from fraim.outputs import sarif
-from fraim.util.chunkers.file_chunker import generate_file_chunks, get_files
 from fraim.util.repo.run_workflows_on_chunks import run_parallel_workflows_on_chunks
 from fraim.util.sarif.write_sarif_report import write_sarif_report
 from fraim.workflows.registry import workflow
@@ -58,11 +58,13 @@ class ApplicationInput:
 
     config: Config
     processes: Annotated[int, {"help": "Number of processes to use"}]
-    # File processing
-    chunk_size: Annotated[int, {"help": "Number of lines per chunk"}] = 500
-    limit: Annotated[Optional[int], {"help": "Limit the number of files to scan"}] = None
+    # Actual Input
     repo: Annotated[Optional[str], {"help": "Repository URL to scan"}] = None
     path: Annotated[Optional[str], {"help": "Local path to scan"}] = None
+    # File processing
+    chunk_size: Annotated[int, {"help": "Number of lines per chunk"}] = 500
+    limit: Annotated[Optional[int], {
+        "help": "Limit the number of files to scan"}] = None
     globs: Annotated[
         Optional[List[str]],
         {"help": "Globs to use for file scanning. If not provided, will use file_patterns defined in the workflow."},
@@ -87,26 +89,20 @@ class ApplicationWorkflow(Workflow[ApplicationInput, ApplicationOutput]):
         results: List[sarif.Result] = []
 
         try:
-            project_path, files_context = get_files(
-                limit=input.limit, repo=input.repo, path=input.path, globs=input.globs or FILE_PATTERNS, config=config
+            kwargs = SimpleNamespace(
+                location=input.repo or input.path,
+                globs=input.globs,
+                limit=input.limit,
+                chunk_size=input.chunk_size
             )
-            # Hack to pass in the project path to the config
-            config.project_path = project_path
+            project = ProjectInput(config=config, kwargs=kwargs)
+            config.project_path = project.project_path
 
-            # Process chunks in parallel as they become available (streaming)
-            with files_context as files:
-                chunks = generate_file_chunks(
-                    config, files=files, project_path=project_path, chunk_size=input.chunk_size
+            results.extend(
+                run_parallel_workflows_on_chunks(
+                    chunks=iter(project), config=config, workflows_to_run=WORKFLOWS_TO_RUN, processes=input.processes, observability_backends=self.observability_backends
                 )
-                results.extend(
-                    run_parallel_workflows_on_chunks(
-                        chunks=chunks,
-                        config=config,
-                        workflows_to_run=WORKFLOWS_TO_RUN,
-                        observability_backends=self.observability_backends,
-                        processes=input.processes,
-                    )
-                )
+            )
 
         except Exception as e:
             config.logger.error(f"Error during scan: {str(e)}")
