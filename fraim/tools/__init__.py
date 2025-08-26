ad8d9bd390f1ed561735cf0891c5712b38ccb8f1
David R. Bild <david.bild@resourcely.io>
2025-08-26 11:59:41 -0400

    tools: expose the new filesystem utilities as tools

--- fraim/tools/__init__.py
+++ fraim/tools/__init__.py
@@ -4,3 +4,8 @@
 """
 Tools that can be made available to LLM agents.
 """
+
+from fraim.tools.filesystem import FilesystemTools
+from fraim.tools.tree_sitter import TreeSitterTools
+
+__all__ = ["FilesystemTools", "TreeSitterTools"]
