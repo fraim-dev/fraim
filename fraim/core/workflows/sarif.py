e531ab3ff48fe22bfa204bac40897a3ef0c82eba
Rudi Theunissen <rtheunissen@resourcely.io>
2025-09-04 13:15:34 -0700

    Organization nits

--- fraim/core/workflows/sarif.py
+++ fraim/core/workflows/sarif.py
@@ -29,6 +29,13 @@ class ConfidenceFilterOptions:
     ] = 7
 
 
+"""
+TODO: Organization nits:
+
+"confidence" isn't actually a SARIF thing (its a custom property that we added). I'd put this in file fraim/core/workflows/mixins/confidence.py.
+
+write_sarif_and_html_report isn't workflow specific. Consider moving it to a fraim/core/ouputs/sarif.py or something like that.
+"""
 def filter_results_by_confidence(results: list[sarif.Result], confidence_threshold: int) -> list[sarif.Result]:
     return [result for result in results if result.properties.confidence > confidence_threshold]
 
